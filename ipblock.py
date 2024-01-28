57105dd5d52f74f8abdac83760561f443f6395c1
Ihor <metalist.pavlenko@gmail.com>
2024-01-28 08:48:35 +0200

    Update ipblock.py

--- ipblock.py
+++ ipblock.py
@@ -1,41 +1,40 @@
-from math import log
-from re import findall
+import math, re
 from urllib.request import Request, urlopen
 
 data = []
 
-url = ['https://ftp.apnic.net/stats/apnic/delegated-apnic-latest',
-       'https://ftp.ripe.net/ripe/stats/delegated-ripencc-latest',
-       'https://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest',
-       'https://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest',
-       'https://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest']
+urls = ['https://ftp.apnic.net/stats/apnic/delegated-apnic-latest',
+        'https://ftp.ripe.net/ripe/stats/delegated-ripencc-latest',
+        'https://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest',
+        'https://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest',
+        'https://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest']
 
-for i in range(len(url)): data += urlopen(Request(url[i])).read().decode('utf-8')
+for url in urls: data += urlopen(Request(url)).read().decode()
 
 blocks = {
     'ipv4':{
         'countries': {},
-        'data': findall('\w+\|(\w+)\|ipv4\|(.+)\|(\d+)\|\d+', ''.join(data))
+        'data': re.findall('\w+\|(\w+)\|ipv4\|(.+)\|(\d+)\|\d+', ''.join(data))
     },
     'ipv6':{
         'countries': {},
-        'data': findall('\w+\|(\w+)\|ipv6\|(.+)\|(\d+)\|\d+', ''.join(data))
+        'data': re.findall('\w+\|(\w+)\|ipv6\|(.+)\|(\d+)\|\d+', ''.join(data))
     },
 }
 
-for ip_type in blocks:
-    for line in blocks[ip_type]['data']:
-        if ip_type == 'ipv4':
-            ip = line[1] + '/' + str(int(32-log(int(line[2]))/log(2)))
+for sort in blocks:
+    for line in blocks[sort]['data']:
+        if sort == 'ipv4':
+            ip = line[1] + '/' + str(int(32-math.log(int(line[2]))/math.log(2)))
         else:
             ip = line[1] + '/' + str(line[2])
 
-        if line[0] not in blocks[ip_type]['countries']:
-            blocks[ip_type]['countries'][line[0]] = []
+        if line[0] not in blocks[sort]['countries']:
+            blocks[sort]['countries'][line[0]] = []
 
-        blocks[ip_type]['countries'][line[0]].append(ip)
+        blocks[sort]['countries'][line[0]].append(ip)
 
-    for country in blocks[ip_type]['countries']:
-        open(country + '_IPv' + ip_type[-1] + '.txt', 'w', encoding = 'utf-8').write(
-            '\n'.join(blocks[ip_type]['countries'][country])
+    for country in blocks[sort]['countries']:
+        open(country + '_IPv' + sort[-1] + '.txt', 'w', encoding = 'utf-8').write(
+            '\n'.join(blocks[sort]['countries'][country])
         )
